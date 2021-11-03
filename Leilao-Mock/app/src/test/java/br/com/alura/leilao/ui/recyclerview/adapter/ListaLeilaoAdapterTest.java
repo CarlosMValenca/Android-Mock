ad103079cd18249527de00a401d65bb162cdc838
Carlos M Valenca <cvalenca@everis.com>
2021-11-03 14:30:35 -0300

    Refatorando o codigo de teste com o Mockito

--- Leilao-Mock/app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
+++ Leilao-Mock/app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -1,56 +1,34 @@
 package br.com.alura.leilao.ui.recyclerview.adapter;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.Display;
 
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import br.com.alura.leilao.model.Leilao;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoAdapterTest {
 
+    @Mock
+    private Context context;
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
+
     @Test
-    public void deve_AtualizarListaDeLeiloes_QuandoReceberListaDeLeiloes(){
-        Context context = Mockito.mock(Context.class);
-        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
-        Mockito.doNothing().when(adapter).atualizaLista();
+    public void deve_AtualizarListaDeLeiloes_QuandoReceberListaDeLeiloes() {
+        doNothing().when(adapter).atualizaLista();
 
         adapter.atualiza(new ArrayList<>(Arrays.asList(
                 new Leilao("Console"),
@@ -58,7 +36,7 @@ public class ListaLeilaoAdapterTest {
         )));
         int quantidadeLeiloesDevolvida = adapter.getItemCount();
 
-        Mockito.verify(adapter).atualizaLista();
+        verify(adapter).atualizaLista();
         assertThat(quantidadeLeiloesDevolvida, is(2));
     }
 
