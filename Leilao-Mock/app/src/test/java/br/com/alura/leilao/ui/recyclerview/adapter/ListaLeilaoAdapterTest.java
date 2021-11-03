d953c6582710067193b0e76b8542c1fa9937b67a
Carlos M Valenca <cvalenca@everis.com>
2021-11-03 13:25:40 -0300

    Testes Android Mocks e Integracoes

---
+++ Leilao-Mock/app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -0,0 +1,64 @@
+package br.com.alura.leilao.ui.recyclerview.adapter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Display;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.com.alura.leilao.model.Leilao;
+
+public class ListaLeilaoAdapterTest {
+
+    @Test
+    public void deve_AtualizarListaDeLeiloes_QuandoReceberListaDeLeiloes(){
+        Context context = Mockito.mock(Context.class);
+        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
+        Mockito.doNothing().when(adapter).atualizaLista();
+
+        adapter.atualiza(new ArrayList<>(Arrays.asList(
+                new Leilao("Console"),
+                new Leilao("Computador")
+        )));
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
+
+        assertThat(quantidadeLeiloesDevolvida, is(2));
+    }
+
+}
