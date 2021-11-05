50e5afa127ccdb2d6a1d22f4134b47573847644e
Carlos M Valenca <cvalenca@everis.com>
2021-11-05 11:18:23 -0300

    Delegando a responsabilidade da busca de leiloes

--- Leilao-Mock/app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeLeiloesTest.java
+++ Leilao-Mock/app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeLeiloesTest.java
@@ -1,7 +1,5 @@
-package br.com.alura.leilao.ui.activity;
+package br.com.alura.leilao.ui;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 
@@ -11,8 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
@@ -27,19 +23,21 @@ import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
 
 @RunWith(MockitoJUnitRunner.class)
-public class ListaLeilaoActivityTest {
+public class AtualizadorDeLeiloesTest {
 
     @Mock
     private ListaLeilaoAdapter adapter;
     @Mock
     private LeilaoWebClient client;
+    @Mock
+    private Context context;
 
     @Test
-    public void deve_AtualizerListaDeLeiloes_QuandoBuscarLeiloesDaApi(){
-        ListaLeilaoActivity activity = new ListaLeilaoActivity();
+    public void deve_AtualizerListaDeLeiloes_QuandoBuscarLeiloesDaApi() {
+        AtualizadorDeLeiloes atualizador = new AtualizadorDeLeiloes();
         doAnswer(new Answer() {
             @Override
-            public Object answer(InvocationOnMock invocation){
+            public Object answer(InvocationOnMock invocation) {
                 RespostaListener<List<Leilao>> argument = invocation.getArgument(0);
                 argument.sucesso(new ArrayList<>(Arrays.asList(
                         new Leilao("Computador"),
@@ -49,7 +47,7 @@ public class ListaLeilaoActivityTest {
             }
         }).when(client).todos(ArgumentMatchers.any(RespostaListener.class));
 
-        activity.buscaLeiloes(adapter, client);
+        atualizador.buscaLeiloes(adapter, client, context);
 
         verify(client).todos(ArgumentMatchers.any(RespostaListener.class));
         verify(adapter).atualiza(new ArrayList<>(Arrays.asList(
